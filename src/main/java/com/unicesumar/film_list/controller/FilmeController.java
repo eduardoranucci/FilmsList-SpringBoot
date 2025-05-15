eb7be395e857e31a08ded9a3b1698739bf74da5b
eduardoranucci <eduardoranucci@outlook.com>
2025-05-15 19:21:48 -0300

    feat: add cadaster page style

--- src/main/java/com/unicesumar/film_list/controller/FilmeController.java
+++ src/main/java/com/unicesumar/film_list/controller/FilmeController.java
@@ -1,6 +1,5 @@
 package com.unicesumar.film_list.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.unicesumar.film_list.model.Filme;
 import com.unicesumar.film_list.model.Usuario;
 import com.unicesumar.film_list.service.FilmeService;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @Controller
 public class FilmeController {
